dc2060d5f0c7cb83568a3c9b5758259f66cfe1ba
ut <ut.numagotatu@gmail.com>
2025-01-26 00:04:09 +0900

    Del commentout

--- portfolio/app/frontend/src/routes/index.tsx
+++ portfolio/app/frontend/src/routes/index.tsx
@@ -20,22 +20,6 @@ const Index = () => {
       </div>
     </>
   )
-  /*
-  return (
-    <>
-      <div className="bg-top-image bg-cover bg-left-top bg-no-repeat w-screen">
-        <div className="py-24 items-center justify-center">
-          <h1 className="p-5 text-8xl text-slate-200 text-center font-mplus1p bg-teal-500/70">
-            Optimizing Digital Experiences
-          </h1>
-          <h2 className="p-3 text-4xl text-slate-200 text-center font-mplus1p bg-orange-500/70">
-            - ut's portfolio website -
-          </h2>
-        </div>
-      </div>
-    </>
-  )
-  */
 }
 
 export const Route = createFileRoute('/')({
